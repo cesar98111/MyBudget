0fc7b81053c983b5b4029d382d0770641a939b91
cesar98111 <cesilius98@gmail.com>
2022-11-17 09:49:32 +0000

    Feature: implementar flatList

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { useState } from 'react';
 import Balance from './component/balance';
+import ItemTrasaction from './component/ItemTrasaction';
 export default function App() {
-  
-
+  const[entry, setEntry] = useState({
+    amount:0,
+    concept:"",
+    date:""
+  })
 
   return (
     <View style={styles.container}>
-      <Balance/>
+      <Balance entry={entry} setEntry={setEntry}/>
       <Text style={styles.balance}></Text>
+      <FlatList data={entry} renderItem={(transactionData)=>{
+        const{key, value} = transactionData.item;
+
+
+        return(
+          <ItemTrasaction style={styles.itemTransaction}
+          key={key}
+          productId={key}
+          entry={transactionData}/>
+        )
+      }}/>
+
+      
+      
     </View>
   );
 }
@@ -25,4 +43,10 @@ const styles = StyleSheet.create({
     alignItems:'center',
     justifyContent: 'flex-start'
   },
+  itemTransaction:{
+    height:30,
+    width:"80%",
+    backgroundColor:"red"
+  }
+
 });
